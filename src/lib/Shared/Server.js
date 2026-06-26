7a104b074aea8b52a77d6c754d3f72b55d1201bf
Safayet <mdsafayet315@gmail.com>
2026-06-26 12:14:52 +0600

    Implement JSON WEB TOKEN verification

--- src/lib/Shared/Server.js
+++ src/lib/Shared/Server.js
@@ -1,20 +1,36 @@
 "use server"
 
+import { headers } from "next/headers";
+import { auth } from "../auth";
+
+
 // import { getUserToken } from "./session";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 export const ServerFetch = async (path)=>{
-    const res =await fetch(`${baseUrl}${path}`)
+     const { token } = await auth.api.getToken({
+    headers: await headers(),
+  });
+    const res =await fetch(`${baseUrl}${path}`,{
+      headers: {
+            authorization: `Bearer ${token}`,
+      },
+    },)
     const data = await res.json();
     return data
 }
 
 
 export const ServerPost =async (formData,path,method ="POST")=>{
+    const { token } = await auth.api.getToken({
+    headers: await headers(),
+  });
+    console.log(token,'Apiii Token');
     const res= await fetch(`${baseUrl}${path}`,{
         method: method,
          headers : {
         "Content-Type": "application/json",
+        authorization : `Bearer ${token}`
         },
          body: JSON.stringify(formData),
     })
